cf54761e1233b480ea130f698589903272b47f83
Travis Hahn <tjxhahn@gmail.com>
2019-01-07 23:10:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var deadWords = "a lot,lots,awesome,cool,big,huge,funny,get,got,good,guy,kid,kind of,sort of,like,mad,nice,sad,scared,things,very,but,so,show,also,I believe, I feel, I think, I know, in my opinion,as to whether,due to the fact that,each and every,equally as,interesting,in terms of,irreguardless,nature,on account of,only,supposed to,use to,try and,this quote shows".split(",");
 var redWordsThird = ["you", "me", "I", "we", "us", "ourselves"];
 
 function getSent() {
@@ -20,23 +19,26 @@ function getSent() {
 }
 
 function getDeadWords() {
+  var deadWords = "a lot,lots,awesome,cool,big,huge,funny,get,got,good,guy,kid,kind of,sort of,like,mad,nice,sad,scared,things,very,but,so,show,also,I believe,I feel,I think,I know,in my opinion,as to whether,due to the fact that,each and every,equally as,interesting,in terms of,irreguardless,nature,on account of,only,supposed to,use to,try and,this quote shows,bad,happy".split(",");
   let essay = document.getElementById('essay').value.split(" ");
+  let essayText = document.getElementById('essay').value;
+  var final = essayText;
+  let num = 0;
   
   if(essay.length == 1) {
     return;
   }
   
-  if (document.getElementById('1').checked === true) {
-    for (let i = 0;i < essay.length;i++) {
-      for (let j = 0;j < redWordsThird.length;j++) {
-      
-      }
+  for (let i = 0;i < deadWords.length;i++) {
+    let replace = deadWords[i];
+    let re = new RegExp(replace, "ig");
+    if (re.test(final)) {
+      num++;
     }
-  } 
-  
-  for (let i = 0;i < essay.length;i++) {
-    for (let j = 0;j < deadWords.length;i++) {
-      
-    }
+    
+    final = final.replace(re, "<" + replace + ">");
   }
+  
+  document.getElementById('essay').value = final;
+  document.getElementById("dead").value = `Dead Word Count: ${num}`;
 }
